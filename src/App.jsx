a64987f6c4123b91ff614f0a14b0a8ec6cf4bab3
Feddy509 <fednelcharite@gmail.com>
2026-05-29 11:10:13 -0400

    integration du composant de notification

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,37 @@ import Navbar from './Components/Navbar/Navbar'
 import Landing_Page from './Components/Landing_Page/Landing_Page'
 import Sign_Up from './Components/Sign_Up/Sign_Up'
 import Login from './Components/Login/Login'
+
+// Importation de tes deux composants de consultation
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
 import BookingConsultation from './Components/BookingConsultation'
 
-// 🎯 NOU ENPÒTE NOTIFICATION AN LA POU L KA SÈVI NAN WOUT LA
+// Importation du composant Notification
 import Notification from './Components/Notification/Notification'
 
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing_Page />} />
-        <Route path="/signup" element={<Sign_Up />} />
-        <Route path="/login" element={<Login />} />
-        
-        {/* 🎯 NOUVO WOUT KONSULTASYON AN: 
-          Nou vlope BookingConsultation anndan Notification jan laboratwa a mande l la
-        */}
-        <Route 
-          path="/instant-consultation" 
-          element={
-            <Notification>
-              <BookingConsultation />
-            </Notification>
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        {/* La Navbar reste visible partout en haut */}
+        <Navbar />
+
+        {/* 🎯 TÂCHE : Notification enveloppe TOUTES les Routes du site */}
+        <Notification>
+          <Routes>
+            <Route path="/" element={<Landing_Page />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Sign_Up />} />
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
+            
+            {/* 🎯 TÂCHE : Remplacement de <component_route> par ton composant personnalisé */}
+            <Route path="/booking-consultation" element={<BookingConsultation />} />
+          </Routes>
+        </Notification>
+      </BrowserRouter>
+    </div>
   )
 }
 
